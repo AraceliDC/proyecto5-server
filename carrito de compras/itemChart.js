2024375f6e67de63e41ed4a82ba23b7d19cb4bec
AraceliDC <agdc.105@gmail.com>
2024-05-20 02:03:52 -0400

    prueba

--- carrito de compras/itemChart.js
+++ carrito de compras/itemChart.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-let cart = [];
+// let cart = [];
 
-router.get('/', (req, res) => {
-  res.json(cart);
-});
+// router.get('/', (req, res) => {
+//   res.json(cart);
+// });
 
-router.post('/add', (req, res) => {
-  const { id, name, price, quantity } = req.body;
-  const item = cart.find(i => i.id === id);
-  if (item) {
-    item.quantity += quantity;
-  } else {
-    cart.push({ id, name, price, quantity });
-  }
-  res.json(cart);
-});
+// router.post('/add', (req, res) => {
+//   const { id, name, price, quantity } = req.body;
+//   const item = cart.find(i => i.id === id);
+//   if (item) {
+//     item.quantity += quantity;
+//   } else {
+//     cart.push({ id, name, price, quantity });
+//   }
+//   res.json(cart);
+// });
 
-router.post('/remove', (req, res) => {
-  const { id } = req.body;
-  cart = cart.filter(item => item.id !== id);
-  res.json(cart);
-});
+// router.post('/remove', (req, res) => {
+//   const { id } = req.body;
+//   cart = cart.filter(item => item.id !== id);
+//   res.json(cart);
+// });
 
-router.post('/clear', (req, res) => {
-  cart = [];
-  res.json(cart);
-});
+// router.post('/clear', (req, res) => {
+//   cart = [];
+//   res.json(cart);
+// });
 
-module.exports = router;
+// module.exports = router;
